07723d40a885bd255870c24d4e25a004756b909f
CircleHsiao <iamshiao@gmail.com>
2015-11-01 23:32:22 +0800

    Upload beta ver proj

---
+++ ClientSideValidation/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ClientSideValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientSideValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person p)
+        {
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ClientSideValidation/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientSideValidation.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }       
+    }
+}
